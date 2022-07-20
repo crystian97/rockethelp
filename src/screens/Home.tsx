2cab1f9067396ca02595a7ab0508dab7309cf4ea
crystian97 <icrystianveloso@gmail.com>
2022-07-20 15:26:46 -0300

    Filter

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,21 @@
-import { HStack, IconButton, useTheme, VStack } from "native-base";
+import { useState } from "react";
+import {
+  Heading,
+  HStack,
+  IconButton,
+  Text,
+  useTheme,
+  VStack,
+} from "native-base";
 import colors from "native-base/lib/typescript/theme/base/colors";
 import { SignOut } from "phosphor-react-native";
 import Logo from "../assets/logo_secondary.svg";
+import { Filter } from "../components/Filter";
 export function Home() {
   const { colors } = useTheme();
+  const [statusSelected, setStatusSelected] = useState<"open" | "closed">(
+    "open"
+  );
   return (
     <VStack flex={1} pb={6} bg="gray.700">
       <HStack
@@ -18,6 +30,33 @@ export function Home() {
         <Logo />
         <IconButton icon={<SignOut size={26} color={colors.gray[300]} />} />
       </HStack>
+      <VStack flex={1} px={6}>
+        <HStack
+          w="full"
+          mt={8}
+          mb={4}
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Heading color="gray.100">Meus chamados</Heading>
+
+          <Text color="gray.200">1</Text>
+        </HStack>
+        <HStack space={3} mb={8}>
+          <Filter
+            title="Em andamentoo"
+            type="open"
+            onPress={() => setStatusSelected("open")}
+            isActive={statusSelected === "open"}
+          />
+          <Filter
+            title="Finalizado"
+            type="close"
+            onPress={() => setStatusSelected("closed")}
+            isActive={statusSelected === "closed"}
+          />
+        </HStack>
+      </VStack>
     </VStack>
   );
 }
