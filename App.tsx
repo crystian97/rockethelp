6ab910d5f6047c463bb265b62c675ceb1429ee8b
crystian97 <icrystianveloso@gmail.com>
2022-07-22 11:15:02 -0300

    Register page and header

--- App.tsx
+++ App.tsx
@@ -7,7 +7,8 @@ import {
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
 import { Loading } from "./src/components/Loading";
-import { Home } from './src/screens/Home';
+import { Home } from "./src/screens/Home";
+import { Register } from "./src/screens/Register";
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
   return (
@@ -17,7 +18,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Home/> : <Loading />}
+      {fontsLoaded ? <Register /> : <Loading />}
     </NativeBaseProvider>
   );
 }
